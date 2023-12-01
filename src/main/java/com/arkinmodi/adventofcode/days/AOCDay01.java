4d77cfd53514c89facd1a8a26c42a695a1d1261b
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-12-01 17:15:04 -0500

    shortcut to not always walk whole line

--- src/main/java/com/arkinmodi/adventofcode/days/AOCDay01.java
+++ src/main/java/com/arkinmodi/adventofcode/days/AOCDay01.java
@@ -8,18 +8,27 @@ public class AOCDay01 implements AOCDay {
     @Override
     public int part1(List<String> input) {
         int calibrationSum = 0;
+
         for (String line : input) {
             char firstDigit = ' ';
             char lastDigit = ' ';
+
             for (int i = 0; i < line.length(); i++) {
                 char c = line.charAt(i);
                 if (isNumber(c)) {
-                    if (firstDigit == ' ') {
-                        firstDigit = c;
-                    }
-                    lastDigit = c;
+                    firstDigit = c;
+                    break;
                 }
             }
+
+            for (int i = line.length() - 1; 0 <= i; i--) {
+                char c = line.charAt(i);
+                if (isNumber(c)) {
+                    lastDigit = c;
+                    break;
+                }
+            }
+
             calibrationSum += Integer.parseInt(String.valueOf(new char[] {firstDigit, lastDigit}));
         }
         return calibrationSum;
@@ -31,16 +40,25 @@ public class AOCDay01 implements AOCDay {
         for (String line : input) {
             char firstDigit = ' ';
             char lastDigit = ' ';
+
             for (int i = 0; i < line.length(); i++) {
                 char c = line.charAt(i);
                 char digit = isNumber(c) ? c : getNumberWord(line, i);
                 if (digit != ' ') {
-                    if (firstDigit == ' ') {
-                        firstDigit = digit;
-                    }
-                    lastDigit = digit;
+                    firstDigit = digit;
+                    break;
                 }
             }
+
+            for (int i = line.length() - 1; 0 <= i; i--) {
+                char c = line.charAt(i);
+                char digit = isNumber(c) ? c : getNumberWord(line, i);
+                if (digit != ' ') {
+                    lastDigit = digit;
+                    break;
+                }
+            }
+
             calibrationSum += Integer.parseInt(String.valueOf(new char[] {firstDigit, lastDigit}));
         }
         return calibrationSum;
@@ -54,9 +72,9 @@ public class AOCDay01 implements AOCDay {
         List<String> digits =
                 List.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
         for (int i = 0; i < digits.size(); i++) {
-            if (start + digits.get(i).length() - 1 < line.length()
-                    && line.substring(start, start + digits.get(i).length())
-                            .equals(digits.get(i))) {
+            String digit = digits.get(i);
+            if (start + digit.length() - 1 < line.length()
+                    && line.substring(start, start + digit.length()).equals(digit)) {
                 return Character.forDigit(i + 1, 10);
             }
         }
