cc551c6633a6450d6d01c651aa4cec6dab7e4103
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-12-03 19:53:17 -0500

    remove extra parentheses and generate directions list

--- src/main/java/com/arkinmodi/adventofcode/days/AOCDay03.java
+++ src/main/java/com/arkinmodi/adventofcode/days/AOCDay03.java
@@ -11,18 +11,20 @@ public class AOCDay03 implements AOCDay {
     @Override
     public int part1(final List<String> input) {
         int partNumberSum = 0;
-        final List<List<Integer>> directions =
-                List.of(
-                        List.of(1, 0),
-                        List.of(1, 1),
-                        List.of(0, 1),
-                        List.of(-1, 1),
-                        List.of(-1, 0),
-                        List.of(-1, -1),
-                        List.of(0, -1),
-                        List.of(1, -1));
 
         // spotless:off
+        final List<List<Integer>> directions =
+            IntStream.of(-1, 0, 1)
+            .boxed()
+            .map(x ->
+                    IntStream.of(-1, 0, 1)
+                    .boxed()
+                    .map(y -> List.of(x, y))
+                    .filter(xy -> xy != List.of(0, 0))
+                    .toList())
+            .flatMap(List::stream)
+            .toList();
+
         record Coordinate(int row, int col) {};
         // spotless:on
 
@@ -32,16 +34,16 @@ public class AOCDay03 implements AOCDay {
                     final int start = end;
                     end =
                             IntStream.range(end + 1, input.get(row).length())
-                                    .filter((col) -> !isNumber(input.get(row).charAt(col)))
+                                    .filter(col -> !isNumber(input.get(row).charAt(col)))
                                     .findFirst()
                                     .orElse(input.get(row).length());
 
                     // spotless:off
                     final boolean isValid = IntStream.range(start, end).anyMatch(
-                            (col) -> {
+                            col -> {
                                 return directions.stream()
-                                    .map((d) -> new Coordinate(row + d.get(0), col + d.get(1)))
-                                    .anyMatch((coord) -> {
+                                    .map(d -> new Coordinate(row + d.get(0), col + d.get(1)))
+                                    .anyMatch(coord -> {
                                         return 0 <= coord.row()
                                             && coord.row() < input.size()
                                             && 0 <= coord.col()
@@ -62,18 +64,20 @@ public class AOCDay03 implements AOCDay {
     @Override
     public int part2(final List<String> input) {
         int partNumberSum = 0;
-        final List<List<Integer>> directions =
-                List.of(
-                        List.of(1, 0),
-                        List.of(1, 1),
-                        List.of(0, 1),
-                        List.of(-1, 1),
-                        List.of(-1, 0),
-                        List.of(-1, -1),
-                        List.of(0, -1),
-                        List.of(1, -1));
 
         // spotless:off
+        final List<List<Integer>> directions =
+            IntStream.of(-1, 0, 1)
+            .boxed()
+            .map(x ->
+                    IntStream.of(-1, 0, 1)
+                    .boxed()
+                    .map(y -> List.of(x, y))
+                    .filter(xy -> xy != List.of(0, 0))
+                    .toList())
+            .flatMap(List::stream)
+            .toList();
+
         record Coordinate(int row, int col) {};
         // spotless:on
 
@@ -84,8 +88,8 @@ public class AOCDay03 implements AOCDay {
 
                     // spotless:off
                     final List<Integer> gears = directions.stream()
-                        .map((d) -> new Coordinate(row + d.get(0), col + d.get(1)))
-                        .filter((coord) -> {
+                        .map(d -> new Coordinate(row + d.get(0), col + d.get(1)))
+                        .filter(coord -> {
                             return 0 <= coord.row()
                                 && coord.row() < input.size()
                                 && 0 <= coord.col()
@@ -93,18 +97,18 @@ public class AOCDay03 implements AOCDay {
                                 && isNumber(input.get(coord.row()).charAt(coord.col()))
                                 && !visited.contains(coord);
                         })
-                        .map((coord) -> {
+                        .map(coord -> {
                             final int start = IntStream
                                 .range(0, coord.col())
                                 .boxed()
                                 .sorted(Collections.reverseOrder())
-                                .filter((c) -> !isNumber(input.get(coord.row()).charAt(c)))
+                                .filter(c -> !isNumber(input.get(coord.row()).charAt(c)))
                                 .findFirst()
                                 .orElse(-1) + 1;
 
                             final int end = IntStream
                                 .range(coord.col(), input.get(coord.row()).length())
-                                .filter((c) -> !isNumber(input.get(coord.row()).charAt(c)))
+                                .filter(c -> !isNumber(input.get(coord.row()).charAt(c)))
                                 .findFirst()
                                 .orElse(input.get(row).length());
 
